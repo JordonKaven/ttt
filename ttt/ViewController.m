3d74b645af80a88a242f2397bc4c4dc915cdb084
Endian <330312471@qq.com>
2016-06-30 15:31:33 +0800

    add a question

--- ttt/ViewController.m
+++ ttt/ViewController.m
@@ -28,6 +28,12 @@
     NSLog(@"%ld  %ld",&str1,&str2);
   }
  //But I still don't know why.
+ //Who can help me?
+  
+  UILabel *testLabel = [[UILabel alloc] init];
+  testLabel.text = @"抱歉，该笔订单已被商户取消,\n您无需进行配送。";
+  CGRect rect = [testLabel.text boundingRectWithSize:CGSizeMake(CGFLOAT_MAX, 19) options:NSStringDrawingUsesLineFragmentOrigin attributes:@{NSFontAttributeName:@13} context:nil];
+  NSLog(@"%@",NSStringFromCGRect(rect));
 }
 
 - (void)didReceiveMemoryWarning {
